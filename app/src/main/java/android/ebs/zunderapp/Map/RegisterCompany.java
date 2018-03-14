f32acf4e2d35f77a4d6f8ac5bd5719fbea76b6aa
PurpleHat <31220861+CarlosRoldanx@users.noreply.github.com>
2018-03-14 23:56:20 +0000

    DAO registration added
    Layout for DAO registration when choosing the location to establish the association has been added.

---
+++ app/src/main/java/android/ebs/zunderapp/Map/RegisterCompany.java
@@ -0,0 +1,14 @@
+package android.ebs.zunderapp.Map;
+
+import android.ebs.zunderapp.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterCompany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_company);
+    }
+}
